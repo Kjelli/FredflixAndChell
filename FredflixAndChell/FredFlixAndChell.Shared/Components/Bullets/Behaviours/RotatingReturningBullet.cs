a81f96109d52f970f46677f1ddb4300c4ff71102
Andreas Aronsen Nesheim <andreas.nesheim@capgemini.com>
2018-11-24 12:45:21 +0100

    added new bullet behaviour: RotatingReturningBullet (boomerang style 8-))

---
+++ FredflixAndChell/FredFlixAndChell.Shared/Components/Bullets/Behaviours/RotatingReturningBullet.cs
@@ -0,0 +1,19 @@
+﻿using FredflixAndChell.Shared.GameObjects.Bullets;
+using Nez;
+using System;
+
+namespace FredflixAndChell.Shared.Components.Bullets.Behaviours
+{
+    public class RotatingReturningBullet : ReturningBullet
+    {
+        public RotatingReturningBullet(Bullet bullet) : base(bullet)
+        {
+        }
+
+        public override void update()
+        {
+            base.update();
+            _bullet.rotation += 8 * (float)Math.PI * Time.deltaTime;
+        }
+    }
+}
