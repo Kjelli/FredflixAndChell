896296a75baa213de21804034e91352614d27b4a
Kjell Arne Hellum <kjell-arne.hellum@capgemini.com>
2019-02-19 22:38:52 +0100

    dont show ctf zones if not ctf gamemode

--- FredflixAndChell/FredflixAndChell.Shared/Maps/MapBuilders/ZoneBuilder.cs
+++ FredflixAndChell/FredflixAndChell.Shared/Maps/MapBuilders/ZoneBuilder.cs
@@ -1,13 +1,10 @@
 ﻿using FredflixAndChell.Shared.GameObjects.Props;
 using FredflixAndChell.Shared.Maps.Events;
+using FredflixAndChell.Shared.Systems.GameModeHandlers;
+using FredflixAndChell.Shared.Utilities;
 using Microsoft.Xna.Framework;
 using Nez;
 using Nez.Tiled;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static FredflixAndChell.Shared.Assets.Constants;
 
 namespace FredflixAndChell.Shared.Maps.MapBuilders
@@ -18,12 +15,23 @@ namespace FredflixAndChell.Shared.Maps.MapBuilders
         {
             foreach (var zoneObject in objectGroup.objectsWithType(TiledObjects.Zone))
             {
+                var props = zoneObject.properties;
+
+                if (props.ContainsKey("ctf_only") && props["ctf_only"] != null)
+                {
+                    var ctfOnly = bool.Parse(props["ctf_only"]);
+                    var currentGameMode = ContextHelper.GameSettings.GameMode;
+                    if (ctfOnly && currentGameMode != GameMode.CTF)
+                    {
+                        continue;
+                    }
+                }
+
                 var color = new Color();
                 var glow = false;
                 var size = new Vector2(zoneObject.width, zoneObject.height);
 
-                var props = zoneObject.properties;
-                if(props.ContainsKey("color") && props["color"] != null)
+                if (props.ContainsKey("color") && props["color"] != null)
                 {
                     color = ColorExt.hexToColor(props["color"].Substring(3));
                 }
@@ -43,8 +51,8 @@ namespace FredflixAndChell.Shared.Maps.MapBuilders
                         EventTriggered = mapEvent =>
                         {
                             var targetColor = Color.Purple;
-                            var targetColorString = (string) mapEvent.Parameters[0];
-                            if(props.ContainsKey(targetColorString) && props[targetColorString] != null)
+                            var targetColorString = (string)mapEvent.Parameters[0];
+                            if (props.ContainsKey(targetColorString) && props[targetColorString] != null)
                             {
                                 targetColor = ColorExt.hexToColor(props[targetColorString].Substring(3));
                             }
